fd5fdf8eed764ec18f57f57974b9146cfbda6732
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-02-28 11:05:49 -0500

    added variables

--- main.lua
+++ main.lua
@@ -1,7 +1,11 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+--Title: Local variables
+--Name: Ryoma Scott
+-- Course: ICS2O
+--This program displays different shapes
 
--- Your code here
+--Create my local variables
+local areaTextPentagon
+local areaTextPolygon
+local areaTextTriangle
+local textSize
+local myPentagon
