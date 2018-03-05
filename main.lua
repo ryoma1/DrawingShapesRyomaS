afe4b27417deac9bf3cfcba5266662044ed88694
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-03-05 11:05:45 -0500

    yes

--- main.lua
+++ main.lua
@@ -3,39 +3,65 @@
 -- Course: ICS2O
 --This program displays different shapes
 
---Create my local variables
-local areaTextPentagon
-local areaTextRoundedRect
-local areaTextTriangle
-local textSize
-local myPentagon
+
+--hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+--create my local variables
+local areaText
+local textSize = 25 
 
 display.setDefault ("background", 0/255, 255/255, 170/255)
 
-local halfW = display.contentWidth * 0.5
-local halfH = display.contentHeight * 0.5
- 
-local vertices = { -50,20, -120,-45, -40,-45, }
- 
-local o = display.newPolygon( halfW, halfH, vertices )
 
-o.strokeWidth = 10
-o:setStrokeColor( 1, 0, 0 )
+--create a rounded rectangle
+local myRoundedRect = display.newRoundedRect(100, 55, 150, 100, 24)
 
-local myRoundedRect = display.newRoundedRect( 100, 50, 150, 50, 12 )
-myRoundedRect.strokeWidth = 3
-myRoundedRect:setFillColor( 3 )
-myRoundedRect:setStrokeColor( 1, 0, 0 )
+myRoundedRect.strokeWidth = 7
+myRoundedRect:setFillColor(1, 1, 1)
+myRoundedRect:setStrokeColor( 0/255, 100/255, 255/255)
 
-local verticiesNani = { 35,-110, 50,-40, 105,-35, 100,16, 30,60, }
-local kachow = display.newPolygon(100, 100, verticiesNani)
+areaText = display.newText("Rounded Rectangle", 0, 0, Arial, textSize)
+
+areaText.anchorX = 0
+areaText.anchorY = 0
+areaText.x = 15
+areaText.y = 10*13
 
 
-kachow.strokeWidth = 3
-kachow:setFillColor( 3 )
-kachow:setFillColor( 3 )
+local tri_vert = {0,2*25, 2*25,-1*25, -2*25,-1*25}
+local triangle = display.newPolygon(175, 175, tri_vert)
 
-kachow.anchorX = 0
-kachow.anchorY = 0
-kachow.x = 20
-kachow.y = 20
+triangle.anchorX = 0
+triangle.anchorY = 0
+triangle.x = 200
+triangle.y = 200
+
+triangle:setStrokeColor(100/255, 6/255, 100/255)
+triangle.strokeWidth = 10
+triangle:setFillColor(255/255, 0/255, 255/255)
+
+areaText = display.newText("Triangle", 0, 0, Arial, textSize)
+
+areaText.anchorX = 0
+areaText.anchorY = 0
+areaText.x = 215
+areaText.y = 300
+
+local pent_vert = {0,2*36, 2*36,0, 2*36,-2*36, -2*36,-2*36, -2*36,0}
+local pentagon = display.newPolygon(175, 175, pent_vert)
+
+pentagon.anchorX = 0
+pentagon.anchorY = 0
+pentagon.x = 300
+pentagon.y = 0
+
+pentagon:setStrokeColor(100/255, 100/255, 255/255)
+pentagon.strokeWidth = 10
+pentagon:setFillColor(0/255, 255/255, 255/255)
+
+areaText = display.newText("Pentagon", 0, 0, Arial, textSize)
+
+areaText.anchorX = 0
+areaText.anchorY = 0
+areaText.x = 325
+areaText.y = 10*15
